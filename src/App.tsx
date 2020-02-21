46d8d3e57180cf8aad93a54aca196ee90115633a
Damien Assany <damienassany@stormadmins-iMac.local>
2020-02-21 11:34:54 +0000

    first comit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+export const App = () => (
+    <div>
+        <h1>Title</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi tempore, ipsam, sed earum molestiae voluptas quam quia quis sit, illo ipsa blanditiis voluptate soluta cum id ad reprehenderit dolorem deleniti!</p>
+    </div>
+)
